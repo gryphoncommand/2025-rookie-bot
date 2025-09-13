b90d3dfff7036c45a9880da2a8697e4aea1578cb
Chris Lunsford <cm@lunsford.io>
2025-09-13 13:03:27 -0400

    auto speed and roller change

--- src/main/java/frc/robot/commands/OnePiece.java
+++ src/main/java/frc/robot/commands/OnePiece.java
@@ -8,8 +8,8 @@ import frc.robot.subsystems.CANRollerSubsystem;
 public class OnePiece extends SequentialCommandGroup { 
     public OnePiece(CANDriveSubsystem drivebase, CANRollerSubsystem effector){
         addCommands(
-            new RunCommand(()->drivebase.driveArcade(0.4, 0), drivebase).withTimeout(4),
-            new RunCommand(()->effector.runRoller(0.5, 0), effector).withTimeout(2)
+            new RunCommand(()->drivebase.driveArcade(0.4, 0), drivebase).withTimeout(6),
+            new RunCommand(()->effector.runRoller(0.6, 0), effector).withTimeout(2)
         );
     }
 }
