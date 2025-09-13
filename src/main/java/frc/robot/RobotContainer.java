2cc69b5158e40c0930aaa1d0783591189dadbdd9
Chris Lunsford <cm@lunsford.io>
2025-09-13 13:01:42 -0400

    roller power incress for auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -86,7 +86,7 @@ public class RobotContainer {
     // joystick matches the WPILib convention of counter-clockwise positive
     driveSubsystem.setDefaultCommand(new DriveCommand(
         () -> -driverController.getLeftY() *
-            (driverController.getHID().getRightBumperButton() ? 0.5 : 1),
+            (driverController.getHID().getRightBumperButton() ? 0.7 : 1),
         () -> -driverController.getRightX(),
         driveSubsystem));
 
