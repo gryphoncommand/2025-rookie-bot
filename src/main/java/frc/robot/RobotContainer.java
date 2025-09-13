43e24347b4a3f5db511e62f5ce3c46811046d215
Chris Lunsford <cm@lunsford.io>
2025-09-13 17:01:07 -0400

    Fixed Erro

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,7 +13,6 @@ import frc.robot.Constants.RollerConstants;
 import frc.robot.commands.AutoCommand;
 import frc.robot.commands.DriveCommand;
 import frc.robot.commands.OnePiece;
-import frc.robot.commands.PushOnePiece;
 import frc.robot.commands.RollerCommand;
 import frc.robot.subsystems.CANDriveSubsystem;
 import frc.robot.subsystems.CANRollerSubsystem;
@@ -55,8 +54,6 @@ public class RobotContainer {
     // autoChooser.addOption
     autoChooser.setDefaultOption("OnePiece", new OnePiece(driveSubsystem, rollerSubsystem));
     autoChooser.addOption("Autonomous", new AutoCommand(driveSubsystem));
-
-    autoChooser.addOption("PushOnePiece", new PushOnePiece(driveSubsystem, rollerSubsystem));
   }
 
   /**
